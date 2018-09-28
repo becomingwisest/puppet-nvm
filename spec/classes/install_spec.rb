f4cf83f9b8d61236465425b465749d143ce3d6e7
Christopher Evans <christopher.evans@gannett.com>
2018-09-28 03:18:04 -0700

    Use the dependancies we declare

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -50,7 +50,7 @@ describe 'nvm::install', :type => :class do
                     .with_user('foo')
                     .with_cwd('/home/foo')
                     .with_unless('/usr/bin/test -d nvm_dir/.git')
-                    .with_require('dependencies')
+                    .with_require('the_dependencies')
                     .that_notifies('Exec[git checkout nvm_repo version]')
     }
     it { should contain_exec('git fetch nvm_repo nvm_dir')
