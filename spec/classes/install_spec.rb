8e0706fd4cae698fc70fc6b92ed910e5b665797a
Christopher Evans <christopher.evans@gannett.com>
2018-09-28 19:08:41 -0700

    Try and fix earlier verions

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -13,7 +13,7 @@ describe 'nvm::install', :type => :class do
       :version => 'version',
       :nvm_dir => 'nvm_dir',
       :nvm_repo => 'nvm_repo',
-      :dependencies => 'Package[wget]',
+      :dependencies => 'Package["wget"]',
       :refetch => false
     }
     end
@@ -23,7 +23,7 @@ describe 'nvm::install', :type => :class do
                     .with_user('foo')
                     .with_cwd('/home/foo')
                     .with_unless('/usr/bin/test -d nvm_dir/.git')
-                    .with_require('Package[wget]')
+                    .with_require('Package["wget"]')
                     .that_notifies('Exec[git checkout nvm_repo version]')
     }
     it { should_not contain_exec('git fetch nvm_repo nvm_dir') }
